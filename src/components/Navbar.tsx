017cff47e0755afb04de92060429e1b34e2a396e
Lý Nguyễn Ngọc Duy <118716140+LyDuy244@users.noreply.github.com>
2025-05-22 21:07:31 +0700

    update source code

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+"use client"
+import React, {useEffect} from 'react';
+import {cn} from "@/lib/utils";
+import Link from "next/link";
+import {Menu, X} from "lucide-react";
+
+const navItems = [
+  {
+    name: "Home",
+    href: "#home"
+  },
+  {
+    name: "About",
+    href: "#about"
+  },
+  {
+    name: "Skills",
+    href: "#skills"
+  },
+  {
+    name: "Projects",
+    href: "#projects"
+  },
+  {
+    name: "Contact",
+    href: "#contact"
+  },
+]
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+  return (
+    <nav
+      className={cn("fixed w-full z-40 transition-all duration-300", isScrolled ? "py-3 bg-background/80z backdrop-blur-md shadow-xs" : "py-5")}>
+
+      <div className={"container flex items-center justify-between"}>
+        <Link className={"text-xl font-black text-primary flex items-center"} href="#hero">
+          <span className={"relative z-10"}>
+            <span className={"text-glow text-foreground"}>Ngoc Duy</span> Portfolio
+          </span>
+        </Link>
+        {/*  desktop nav*/}
+        <div className={"hidden md:flex space-x-8"}>
+          {
+            navItems.map((item, key) => (
+              <Link key={key} href={item.href}
+                    className={"text-foreground/80 hover:text-primary transition-colors duration-300"}>
+                {item.name}
+              </Link>
+            ))
+          }
+        </div>
+        {/*  mobile nav*/}
+        <button className={"md:hidden cursor-pointer p-2 text-foreground z-50"}
+                aria-label={isScrolled ? "Close Menu" : "Open Menu"}
+                onClick={() => setIsMenuOpen(!isMenuOpen)}>{isMenuOpen ? <X size={24}/> : <Menu size={24}/>}</button>
+        <div
+          className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+          )}>
+          <div className={"flex flex-col space-y-8 text-xl"}>
+            {
+              navItems.map((item, key) => (
+                <Link key={key} href={item.href}
+                      className={"text-foreground/80 hover:text-primary transition-colors duration-300"}
+                      onClick={() => setIsMenuOpen(false)}
+                >
+                  {item.name}
+                </Link>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
